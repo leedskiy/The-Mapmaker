e0d8174485bc0de1ff06f135efed498aeaa67567
Illia Leedskiy <leedqvo@gmail.com>
2023-11-05 23:48:57 +0100

    write css and add fixes

--- js/script.js
+++ js/script.js
@@ -5,6 +5,18 @@ for (let i = 1; i <= 121; ++i) {
     let newElem = document.createElement('button');
     newElem.classList.add('grid__elem');
     newElem.setAttribute('data-index', `${i}`);
+    if (i === 1) {
+        newElem.style.cssText = `border-top-left-radius: 5px;`;
+    }
+    else if (i === 11) {
+        newElem.style.cssText = `border-top-right-radius: 5px;`;
+    }
+    else if (i === 111) {
+        newElem.style.cssText = `border-bottom-left-radius: 5px;`;
+    }
+    else if (i === 121) {
+        newElem.style.cssText = `border-bottom-right-radius: 5px;`;
+    }
     gridContainer.append(newElem);
 }
 
