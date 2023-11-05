ff23547a8369be421fb317d8df10871ec5dfbbfe
Illia Leedskiy <leedqvo@gmail.com>
2023-11-05 19:57:25 +0100

    finish html

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
 let gridContainer = document.querySelector('.grid__container');
+let s3gridContainer = document.querySelector('.s3grid__container');
 
-for (let i = 1; i <= 121; i++) {
+for (let i = 1; i <= 121; ++i) {
     let newElem = document.createElement('button');
     newElem.classList.add('grid__elem');
     newElem.setAttribute('data-index', `${i}`);
     gridContainer.append(newElem);
+}
+
+for (let i = 1; i <= 9; ++i) {
+    let newElem = document.createElement('button');
+    newElem.classList.add('s3grid__elem');
+    newElem.setAttribute('data-index', `${i}`);
+    s3gridContainer.append(newElem);
 }
