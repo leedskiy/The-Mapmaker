de832f860653ea1c705fb4e9a5467d129ce5debd
Illia Leedskiy <leedqvo@gmail.com>
2023-11-11 00:53:22 +0100

    write gameController js

---
+++ js/controllers/gameController.js
@@ -0,0 +1,26 @@
+import { GridClass } from "../grid/grid.js"
+import { Element } from "../element/element.js"
+import { elements } from "../data/elements.js"
+import { displayController } from "./displayController.js"
+
+export const gameController = (() => {
+    let currElem = new Element(elements[0].time, elements[0].type, elements[0].shape,
+        elements[0].rotation, elements[0].mirrored);
+    let grid = new GridClass();
+
+    const getCurrElem = () => {
+        return currElem;
+    }
+
+    const getGrid = () => {
+        return grid;
+    }
+
+    const startGame = () => {
+        displayController.createHtmlGrid();
+        displayController.setCurrElementHtml();
+    }
+
+    return { getCurrElem, getGrid, startGame }
+})();
+
