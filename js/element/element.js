77e0628baeef692bbb0fd52cd2f36f3ca42884c4
Illia Leedskiy <leedqvo@gmail.com>
2023-11-11 00:51:31 +0100

    finish element class js

---
+++ js/element/element.js
@@ -0,0 +1,58 @@
+export class Element {
+    #time;
+    #type;
+    #shape;
+    #rotation;
+    #mirrored;
+
+    constructor(time, type, shape, rotation, mirrored) {
+        this.#time = time;
+        this.#type = type;
+        this.#shape = shape;
+        this.#rotation = rotation;
+        this.#mirrored = mirrored;
+    }
+
+    getTime = () => {
+        return this.#time;
+    }
+
+    getType = () => {
+        return this.#type;
+    }
+
+    getShape = () => {
+        return this.#shape;
+    }
+
+    getRotation = () => {
+        return this.#rotation;
+    }
+
+    getMirrored = () => {
+        return this.#mirrored;
+    }
+
+    getCellsHtml = () => {
+        const cellsHtml = [];
+
+        for (let i = 0; i < this.#shape.length; ++i) {
+            for (let j = 0; j < this.#shape[i].length; j++) {
+                let cellHtml;
+
+                if (this.#shape[i][j] === 1) {
+                    cellHtml = `<img class="s3grid__elem gelem" src="img/tiles/${this.#type}_tile.png"` +
+                        `alt="${this.#type}_cell" data-row="${i}" data-col="${j}">`
+                }
+                else {
+                    cellHtml = `<img class="s3grid__elem gelem" src="img/tiles/base_tile.png"` +
+                        `alt="base_cell" data-row="${i}" data-col="${j}">`
+                }
+
+                cellsHtml.push(cellHtml);
+            }
+        }
+
+        return cellsHtml;
+    }
+}
