b4c02104ff586da9ae648688597fa25fc8e5a216
Illia Leedskiy <leedqvo@gmail.com>
2023-11-11 00:49:49 +0100

    write cell and grid js files

---
+++ js/grid/cell.js
@@ -0,0 +1,23 @@
+export class Cell {
+    #row;
+    #col;
+    #type;
+
+    constructor(row, col, type) {
+        this.#row = row;
+        this.#col = col;
+        this.#type = type;
+    }
+
+    getRow = () => {
+        return this.#row;
+    }
+
+    getCol = () => {
+        return this.#col;
+    }
+
+    getType = () => {
+        return this.#type;
+    }
+}
---
+++ js/grid/grid.js
@@ -0,0 +1,27 @@
+import { Cell } from "./cell.js"
+
+export class GridClass {
+    #grid;
+
+    constructor() {
+        this.#grid = [];
+        this.initializeGrid();
+    }
+
+    initializeGrid = () => {
+        for (let i = 0; i < 11; i++) {
+            let row = [];
+
+            for (let j = 0; j < 11; j++) {
+                let cell = new Cell(i, j, "base");
+                row.push(cell);
+            }
+
+            this.#grid.push(row);
+        }
+    }
+
+    getGrid = () => {
+        return this.#grid;
+    }
+}
